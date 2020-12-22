2006ba79ea6fe4c9565c86624b5bbcae5c1aa04e
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2020-12-23 00:14:10 +0700

    #2 remove linkage of static libs into kotlin lib

---
+++ MCRCDynamicProxy.podspec
@@ -0,0 +1,15 @@
+Pod::Spec.new do |spec|
+    spec.name                     = 'MCRCDynamicProxy'
+    spec.version                  = '0.1.0'
+    spec.homepage                 = 'https://github.com/icerockdev/moko-crash-reporting'
+    spec.source                   = { :git => "https://github.com/icerockdev/moko-crash-reporting.git", :tag => "release/#{spec.version}" }
+    spec.authors                  = 'IceRock Development'
+    spec.license                  = { :type => 'Apache 2', :file => 'LICENSE.md' }
+    spec.summary                  = 'summary'
+    spec.module_name              = "#{spec.name}"
+
+    spec.ios.deployment_target  = '11.0'
+    spec.swift_version = '5'
+
+    spec.source_files = "crash-reporting-crashlytics/src/iosMain/swift/DynamicProxy/**/*.{h,m,swift}"
+end
---
+++ crash-reporting-crashlytics/src/iosMain/swift/StaticReporter/FirebaseStaticReporter.swift
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2020 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+import FirebaseCrashlytics
+import MCRCDynamicProxy
+
+public class MokoFirebaseCrashlytics: FirebaseCrashlyticsReporter {
+  public func setUserId(userId: String) {
+    Crashlytics.crashlytics().setUserID(userId)
+  }
+  
+  public func setCustomValue(value: String, forKey: String) {
+    Crashlytics.crashlytics().setCustomValue(value, forKey: forKey)
+  }
+  
+  public func recordException(name: String, reason: String, stackTrace: [UInt]) {
+    let exceptionModel = ExceptionModel(
+      name: name,
+      reason: reason
+    )
+    let stackFrames = stackTrace.map {
+      StackFrame(address: $0)
+    }
+    
+    exceptionModel.stackTrace = stackFrames
+    
+    Crashlytics.crashlytics().record(exceptionModel: exceptionModel)
+  }
+  
+  public func log(message: String) {
+    Crashlytics.crashlytics().log(message)
+  }
+  
+  public static func setup() {
+    FirebaseDynamicProxy.reporter = MokoFirebaseCrashlytics()
+  }
+}
